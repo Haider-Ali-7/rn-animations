2962355ffa84de27d3ac2637d9be436b5e34ed5e
Haider-Ali-7 <haideralitariqcheema@gmail.com>
2026-07-10 01:53:03 +0500

    updated viewable item animation

--- src/components/ListItem2.tsx
+++ src/components/ListItem2.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StyleSheet, ViewToken } from 'react-native';
-import Animated, { SharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import Animated, {
+  cancelAnimation,
+  SharedValue,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
 
 interface ListItem2Props {
   viewableItems: SharedValue<ViewToken[]>;
@@ -8,32 +15,38 @@ interface ListItem2Props {
 }
 
 const ListItem2: React.FC<ListItem2Props> = ({ viewableItems, item }) => {
-  const rViewStyle = useAnimatedStyle(() => {
-    const isVisible = Boolean(viewableItems.value.some(el => el.item.id == item.id));
+  const opacity = useSharedValue(0);
+  const scale = useSharedValue(0.6);
 
-    return {
-      opacity: withTiming(isVisible ? 1 : 0),
-      transform: [{ scale: withTiming(isVisible ? 1 : 0.6) }]
-    };
-  });
-
-  return (
-    <Animated.View
-      style={[
-        {
-          height: 80,
-          width: '90%',
-          backgroundColor: '#78cad2',
-          alignSelf: 'center',
-          borderRadius: 15,
-          marginTop: 20
-        },
-        rViewStyle
-      ]}
-    />
+  useAnimatedReaction(
+    () => viewableItems.value.some(el => el.item.id === item.id),
+    (isVisible, wasVisible) => {
+      if (isVisible !== wasVisible) {
+        cancelAnimation(opacity);
+        cancelAnimation(scale);
+        opacity.value = withTiming(isVisible ? 1 : 0);
+        scale.value = withTiming(isVisible ? 1 : 0.6);
+      }
+    }
   );
+
+  const rViewStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    transform: [{ scale: scale.value }]
+  }));
+
+  return <Animated.View style={[styles.item, rViewStyle]} />;
 };
 
 export default React.memo(ListItem2);
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    height: 80,
+    width: '90%',
+    backgroundColor: '#78cad2',
+    alignSelf: 'center',
+    borderRadius: 15,
+    marginTop: 20
+  }
+});
